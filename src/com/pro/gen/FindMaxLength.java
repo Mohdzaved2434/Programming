a0325c5839e8b01201fb276c7a913bb7eea1f57a
ts-zaved.mohd <April@123>
2022-04-23 00:20:35 +0900

    * equals and hashcode functions

--- src/com/pro/gen/FindMaxLength.java
+++ src/com/pro/gen/FindMaxLength.java
@@ -9,9 +9,6 @@ public class FindMaxLength {
         int[] B = {1, 3, 4, 2, 6};
         System.out.println(findMaxLenght(B));
 
-        System.out.println(findMax(A));
-        System.out.println(findMax(B));
-
     }
 
     public static int findMaxLenght(int[] A) {
@@ -25,8 +22,8 @@ public class FindMaxLength {
                 max_len = Math.max(max_len, i - start);
                 start = i - 1;
                 if (i % 2 == 0) {
-                    odd = A[i - 1];
                     even = A[i];
+                    odd = A[i - 1];
                 } else {
                     even = A[i - 1];
                     odd = A[i];
@@ -34,32 +31,5 @@ public class FindMaxLength {
             }
         }
         return Math.max(max_len, A.length - start);
-    }
-
-
-    public static int findMax(int[] input) {
-        if (input.length <= 2) {
-            return input.length;
-        }
-        int even = input[0];
-        int odd = input[1];
-
-        int start = 0;
-        int end = 0;
-
-        for (int i = 2; i < input.length; i++) {
-            if (i % 2 == 0 && even != input[i] || i % 2 == 1 && odd != input[i]) {
-                end = Math.max(end, i - start);
-                start = i - 1;
-                if (i % 2 == 0) {
-                    odd = input[i - 1];
-                    even = input[i];
-                } else {
-                    even = input[i - 1];
-                    odd = input[i];
-                }
-            }
-        }
-        return Math.max(end, input.length - start);
     }
 }
